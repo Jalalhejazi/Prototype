74761918447b74d9d86d6b80e507d850b5a4f49a
Jalal Hejazi <jalal.hejazi@gmail.com>
2013-05-02 10:20:53 +0200

    1. prototype version

---
+++ program.js
@@ -0,0 +1,26 @@
+﻿
+// Lommeregner 1.klasse
+
+// OOP > constructor
+var Calculator = function (x , y) {
+    this.x = x ;
+    this.y = y;
+};
+
+//child
+Calculator.prototype = {
+    plus: function (x, y) { return x + y }
+    ,
+    minus: function (x, y) { return x - y }
+    ,
+    sin: function (x) { Math.sin(x) }
+
+};
+
+
+//Usage: 
+var calc = new Calculator(0, 0);
+
+console.log("forventer plus= 30" , calc.plus(10, 20) ) ;
+
+
